c087d2c7001024299093b7bafeae407d2f3a30ca
Simon Egebjerg Jensen <simo897d@edu.ucl.dk>
2019-11-19 12:09:31 +0100

    Lavet user dashboard. Hvis man ikke har en subscription(Denne skal laves om fra bool til noget mere scalerbart) kommer man ind på en side der er beregnet til at skulle købe subscriptions(Subscriptions information mangler, skal hentes ned fra API)

--- VitecMVC3/Models/ApplicationUser.cs
+++ VitecMVC3/Models/ApplicationUser.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 
 namespace VitecMVC3.Models
 {
     public class ApplicationUser : IdentityUser {
-        public bool HasProduct {get;set;}
+        
+        public bool HasProduct;
     }
 }
---
+++ VitecMVC3/Models/Subscription.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VitecMVC3.Models
+{
+    public class Subscription {
+        public string SubscriptionType { get; set; }
+        public double Price { get; set; }
+        public DateTime StartDate{get;set;}
+        public DateTime EndDate { get; set; }
+    }
+}
