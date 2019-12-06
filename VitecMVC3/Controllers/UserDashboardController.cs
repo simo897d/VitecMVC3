2e2f1c27e605e76932d3f47317c2d65e9b75b765
Simon Egebjerg Jensen <simo897d@edu.ucl.dk>
2019-12-06 13:21:01 +0100

    Loade Subscriptions ind på BuySubscriptions siden

--- VitecMVC3/Controllers/UserDashboardController.cs
+++ VitecMVC3/Controllers/UserDashboardController.cs
@@ -1,22 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using VitecMVC3.Models;
 
 namespace VitecMVC3.Controllers
 {
     [Authorize(Roles = "User,Admin,Administrator")]
     public class UserDashboardController : Controller
     {
-        public IActionResult Index()
-        {
-           
+        public IActionResult Index() {
+
             return View();
         }
+        [AllowAnonymous]
         public IActionResult BuySubscriptionPage() {
-            return View();
+            using (HttpClient client = new HttpClient()) {
+
+                HttpResponseMessage response = client.GetAsync("https://localhost:44388/api/Subscription").Result;
+                response.EnsureSuccessStatusCode();
+                var responseBody = response.Content.ReadAsStringAsync().Result;
+
+                var model = JsonConvert.DeserializeObject<List<Subscription>>(responseBody);
+                return View(model);
+            }
         }
     }
 }
